8785e0bfb050ff3b86b9d945249e43362b2f5977
xmcbbkad <188135446@qq.com>
2024-09-10 16:58:16 +0800

    add Transformer_predict_price.sh

--- scripts/stock/Transformer_predict_price.sh
+++ scripts/stock/Transformer_predict_price.sh
@@ -1,6 +1,6 @@
 export CUDA_VISIBLE_DEVICES=0
 
-model_name=iTransformer
+model_name=Transformer
 
 python -u run_stock_predict_price.py \
   --is_training 1 \
@@ -14,6 +14,9 @@ python -u run_stock_predict_price.py \
   --label_len 0 \
   --pred_len 30 \
   --e_layers 4 \
+  --enc_in 4 \
+  --dec_in 4 \
+  --c_out 4 \
   --des 'Exp' \
   --d_model 512\
   --d_ff 512 \
